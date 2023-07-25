bff7843f25dd5279569c89ad878244e109e8eca0
morgance <mogeokumu@gmail.com>
2023-07-25 23:16:12 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -39,5 +39,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void recursion_heap(int *arr, int i, size_t size, int limit);
+void swap_nums(int *arr, int a, int b);
 
 #endif
