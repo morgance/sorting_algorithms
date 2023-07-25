11c7f4b886d04b9ee68578df72de440601296bc5
Eodera1 <ericowino8@gmail.com>
2023-07-25 19:18:05 +0000

    Quick sort

--- sort.h
+++ sort.h
@@ -25,8 +25,14 @@ listint_t *swap_node(listint_t **list, listint_t *node);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_num(int *fint, int *sint);
-int partition(int *array, int a, int b, size_t size);
-void lomu(int *array, int a, int b, size_t size);
 void quick_sort(int *array, size_t size);
+void _qsort(int *a, int low, int high, int size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif
