33d1d91523e104c884e93a6ae3fc8901b3481150
Eodera1 <ericowino8@gmail.com>
2023-07-25 19:39:28 +0000

    Cocktail sort list

--- sort.h
+++ sort.h
@@ -30,10 +30,15 @@ void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n)
 void cocktail_sort_list(listint_t **list);
+void increase_sort(listint_t **ptr, listint_t **limit, listint_t **list);
+void decrease_sort(listint_t **ptr, listint_t **limit, listint_t **list)
+void cocktail_sort_list(listint_t **list)
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
