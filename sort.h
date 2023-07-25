f5d9585342bbfbbb4b96174d341d9433f76027a6
Eodera1 <ericowino8@gmail.com>
2023-07-25 19:22:23 +0000

    Shell sort

--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ void swap_num(int *fint, int *sint);
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
+void _ssort(int *a, int size, int n)
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
